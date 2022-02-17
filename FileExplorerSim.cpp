f286977355a3a331db032c365949a29be35332c7
Janujussi <jancameron.morrison@googlemail.com>
2022-02-17 01:15:41 -0500

    Improved user command input

--- FileExplorerSim.cpp
+++ FileExplorerSim.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-// Print current directory
-string printDirectory(Directory* currDirectory);
+string printDirectory(Directory* currDirectory);						// Print current directory
+void split(string stringToSplit, vector<string>& splitStringOutput);	// Split string
 
 int main() {
 	FileExplorer thisSimulation;		// File explorer object
@@ -17,21 +17,29 @@ int main() {
 		"cd"
 	};
 	size_t numOfCommands = sizeof(commands) / sizeof(commands[0]); 	// Size of command list
-	string userCommand;		// Command user enters
+	vector<string> userCommand;		// Command user enters
+	string commandInput;	// Input of command line
+	string dirName;			// Name of directory in command
+	string sCommand;		// User command
 	int command;			// Index of user command
 	bool validCommand;		// Check command is valid flag
 
 	while(true) {
-		userCommand = "";
+		commandInput = "";
+		userCommand.clear();
 		validCommand = false;
 
 		cout << endl << printDirectory(currDirectory) << " >> ";	// Print current directory in command line
-		cin >> userCommand;
+		getline(cin, commandInput); 					// Getting user input
+		split(commandInput, userCommand);
 		cout << endl;
+
+		sCommand = userCommand[0];
+		dirName = userCommand[1];
 
 		// Check command list for command and assign index to command
 		for (int i = 0; i < numOfCommands; i++) {
-			if (userCommand == commands[i]) {
+			if (sCommand == commands[i]) {
 				command = i;
 				validCommand = true;
 			}
@@ -43,10 +51,6 @@ int main() {
 		// Command logic
 		switch(command) {
 			case 0: {	// mkdir
-				string dirName = "";		// Name of new directory
-
-				cout << "Directory name: ";
-				cin >> dirName;
 				if (						// Check if name is valid
 					dirName[0] < 48
 					&& dirName[0] > 57
@@ -65,18 +69,13 @@ int main() {
 			}
 
 			case 1: {	// deldir
-				string dirToDelete = "";
-
-				cout << "Directory name: ";
-				cin >> dirToDelete;
-
 				for (int i = 0; i < currDirectory->getDirSize(); i++) {
-					if (currDirectory->getChildrenDirectories()[i]->getDirName() == dirToDelete) {
+					if (currDirectory->getChildrenDirectories()[i]->getDirName() == dirName) {
 						currDirectory->getChildrenDirectories()[i]->deleteDescendentDirectories();
 					}
 				}
 
-				currDirectory->deleteDirectory(dirToDelete);
+				currDirectory->deleteDirectory(dirName);
 
 				break;
 			}
@@ -88,12 +87,8 @@ int main() {
 			}
 
 			case 3: {	// cd
-				string dirName = "";				// Name of directory to change to
 				string iterateChildName = "";		// Name of child directory to compare to dirName
 				bool changed = false;				// flag for changing directory
-
-				cout << "Directory name: ";
-				cin >> dirName;
 
 				if (dirName == "..") {
 					if (currDirectory->getParentDirectory() == NULL) {
@@ -144,4 +139,19 @@ string printDirectory(Directory* currDirectory) {
 	}
 
 	return directory;
+}
+
+void split(string stringToSplit, vector<string>& splitStringOutput) {
+	int iterateNum = stringToSplit.length();
+	string stringToInsert = "";
+
+	for (int i = 0; i < iterateNum; i++) {
+		if (stringToSplit[i] != ' ') {
+			stringToInsert += stringToSplit[i];
+		} else {
+			splitStringOutput.push_back(stringToInsert);
+			stringToInsert = "";
+		}
+	}
+	splitStringOutput.push_back(stringToInsert);
 }
